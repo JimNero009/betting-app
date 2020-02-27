7fd725c466e8d567f02627f4a85e3fdd422e15b0
Jim Cockburn <jim.cockburn@skyscanner.net>
2020-02-27 16:48:33 +0000

    basic outline

--- betting-returns-calculator/ViewController.swift
+++ betting-returns-calculator/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var betAmount: UITextField!
+    @IBOutlet weak var outputField: UITextField!
+    @IBOutlet weak var oddsLeft: UITextField!
+    @IBOutlet weak var oddsRight: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        betAmount.text = "1"
+        oddsLeft.text = "1"
+        oddsRight.text = "1"
     }
-
+    
+    
+    @IBAction func calculate(_ sender: Any) {
+        let bet = Float(betAmount.text!)!
+        let leftOdd = Float(oddsLeft.text!)!
+        let rightOdd = Float(oddsRight.text!)!
+        let result = bet + bet * leftOdd/rightOdd
+        outputField.text = String(result)
+    }
+    
 
 }
 
